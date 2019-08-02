66a9940c3f6b5ddfcd47f09b2c57d0b6d2d5142a
Pao Perez <pao.pacheco.perez@gmail.com>
2019-08-02 10:37:01 +0800

    Fix: Cors config

--- graphql/src/main/java/com/paoperez/graphql/MainApplication.java
+++ graphql/src/main/java/com/paoperez/graphql/MainApplication.java
@@ -2,6 +2,7 @@ package com.paoperez.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,11 +13,13 @@ public class MainApplication {
 		SpringApplication.run(MainApplication.class, args);
 	}
 
+	
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/graphql/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/graphql").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
