cb0cea72e9738023dd984f37f8823b60d07440d6
Pao Perez <pao.pacheco.perez@gmail.com>
2019-12-03 13:24:02 +0800

    Builder is stateful and stick to per container vs per loader for datetimefactory

--- content-service/src/main/java/com/paoperez/contentservice/ContentConfig.java
+++ content-service/src/main/java/com/paoperez/contentservice/ContentConfig.java
@@ -3,6 +3,7 @@ package com.paoperez.contentservice;
 import com.paoperez.contentservice.util.DateTimeFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 class ContentConfig {
@@ -12,6 +13,7 @@ class ContentConfig {
   }
 
   @Bean
+  @Scope("prototype")
   Content.Builder builder(final DateTimeFactory factory) {
     return new Content.Builder(factory);
   }
