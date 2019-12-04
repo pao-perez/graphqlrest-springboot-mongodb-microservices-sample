a16529c1fa3788fc3f6b5841562d5a2a3362f486
Pao Perez <pao.pacheco.perez@gmail.com>
2019-12-04 12:32:26 +0800

    Initialize discovery server

---
+++ discovery-service/src/main/java/com/paoperez/discoveryservice/MainApplication.java
@@ -0,0 +1,15 @@
+package com.paoperez.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ discovery-service/src/test/java/com/paoperez/discoveryservice/MainApplicationTest.java
@@ -0,0 +1,13 @@
+package com.paoperez.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
