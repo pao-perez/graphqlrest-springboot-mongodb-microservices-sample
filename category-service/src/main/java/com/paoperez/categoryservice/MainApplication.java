52c849257df44e1d3b54259a30565c4e8a0db3dc
Pao Perez <pao.pacheco.perez@gmail.com>
2019-12-11 13:28:12 +0800

    one level up

--- category-service/src/main/java/com/paoperez/categoryservice/MainApplication.java
+++ category-service/src/main/java/com/paoperez/categoryservice/MainApplication.java
@@ -2,10 +2,10 @@ package com.paoperez.categoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MainApplication {
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
