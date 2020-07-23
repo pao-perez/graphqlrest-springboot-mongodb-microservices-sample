3f108ba3166f9d7c2c03426d930faaf2527314c3
Pao Perez <pao.pacheco.perez@gmail.com>
2020-07-23 12:27:31 +0800

    add log messages

--- content-service/src/main/java/com/paoperez/contentservice/MainApplication.java
+++ content-service/src/main/java/com/paoperez/contentservice/MainApplication.java
@@ -1,5 +1,7 @@
 package com.paoperez.contentservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,7 +9,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MainApplication {
+  private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
+
   public static void main(String[] args) {
+    logger.info("Starting Content service...");
     SpringApplication.run(MainApplication.class, args);
   }
 }
--- discovery-service/src/main/java/com/paoperez/discoveryservice/MainApplication.java
+++ discovery-service/src/main/java/com/paoperez/discoveryservice/MainApplication.java
@@ -1,5 +1,7 @@
 package com.paoperez.discoveryservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,8 +9,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class MainApplication {
+  private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
 
   public static void main(String[] args) {
+    logger.info("Starting Discovery service...");
     SpringApplication.run(MainApplication.class, args);
   }
 }
