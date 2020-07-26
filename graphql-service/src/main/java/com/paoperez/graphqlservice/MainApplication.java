2f4c65665c2fc16c0f32fd5cac4b0c1849d8068d
Pao Perez <pao.pacheco.perez@gmail.com>
2020-07-26 19:17:18 +0800

    disable eureka for dev env, create spring profiles and own compose files for graphql service

--- graphql-service/src/main/java/com/paoperez/graphqlservice/MainApplication.java
+++ graphql-service/src/main/java/com/paoperez/graphqlservice/MainApplication.java
@@ -4,10 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MainApplication {
   private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
 
