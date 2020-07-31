69c4d4ed6e416e4d9881545f7620e43796ce02d4
Pao Perez <pao.pacheco.perez@gmail.com>
2020-07-31 13:00:50 +0800

    refactor db script, change db name

--- avatar-service/db/scripts/init.sh
+++ avatar-service/db/scripts/init.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-echo "Creating user ${MONGO_USERNAME} with readWrite access to database ${MONGO_DATABASE}."
-mongo admin --host localhost --port 27017 --eval "db.createUser({user: '${MONGO_USERNAME}', pwd: '${MONGO_PASSWORD}', roles: [{ role: 'readWrite', db: '${MONGO_DATABASE}' }]});"
-# "${mongo[@]}" "admin" <<-EOJS
-# 	db.createUser({
-# 		user: $(_js_escape ${MONGO_USERNAME}),
-# 		pwd: $(_js_escape ${MONGO_PASSWORD}),
-# 		roles: [ { role: 'readWrite', db: '$MONGO_DATABASE' } ]
-# 	})
-# EOJS
-echo "User ${MONGO_USERNAME} with readWrite access to ${MONGO_DATABASE} database created."
+echo "Creating user ${MONGO_USERNAME} with readWrite access to database contentually..."
+mongo admin --eval "db.createUser({ user: '${MONGO_USERNAME}', pwd: '${MONGO_PASSWORD}', roles: [{ role: 'readWrite', db: 'contentually' }]});"
