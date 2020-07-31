b20c4c9bd6c08bd8938cbcd2de13da4b9bc6bc2c
Pao Perez <pao.pacheco.perez@gmail.com>
2020-07-31 12:43:29 +0800

    refactor db script, add initial data

--- image-service/db/scripts/init.sh
+++ image-service/db/scripts/init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-echo "Creating user ${MONGO_USERNAME} with readWrite access to database ${MONGO_DATABASE}."
-mongo admin --host localhost --port 27017 --eval "db.createUser({user: '${MONGO_USERNAME}', pwd: '${MONGO_PASSWORD}', roles: [{ role: 'readWrite', db: '${MONGO_DATABASE}' }]});"
-# "${mongo[@]}" "admin" <<-EOJS
-# 	db.createUser({
-# 		user: $(_js_escape ${MONGO_USERNAME}),
-# 		pwd: $(_js_escape ${MONGO_PASSWORD}),
-# 		roles: [ { role: 'readWrite', db: '$MONGO_DATABASE' } ]
-# 	})
-# EOJS
-echo "User ${MONGO_USERNAME} with readWrite access to ${MONGO_DATABASE} database created."
+echo "Creating user ${MONGO_USERNAME} with readWrite access to database contentually..."
+mongo admin --eval "db.createUser({ user: '${MONGO_USERNAME}', pwd: '${MONGO_PASSWORD}', roles: [{ role: 'readWrite', db: 'contentually' }]});"
+
+echo "Importing image data..."
+echo '{ "name": "300x200", "url": "https://placekitten.com/200/300", "alt": "Kitten at 300x200", "width": 300, "height": 200 }
+      { "name": "150x150", "url": "https://placekitten.com/150/150", "alt": "Kitten at 150x150", "width": 150, "height": 150 }' | 
+      mongoimport --db contentually --collection image \
+      --authenticationDatabase admin --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} \
+      --drop
