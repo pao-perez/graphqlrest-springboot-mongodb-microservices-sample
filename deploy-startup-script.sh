27aaab08ba1ce20a9a60170a2153dbdf217b55a2
Pao Perez <pao.pacheco.perez@gmail.com>
2020-10-14 10:54:23 +0800

    chore: detach from docker process

--- deploy-startup-script.sh
+++ deploy-startup-script.sh
@@ -6,4 +6,4 @@ set -e
 # Get environment values from Metadata server
 DEPLOYMENT_ENV=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/deployment-env" -H "Metadata-Flavor: Google")
 
-DEPLOYMENT_ENV=$DEPLOYMENT_ENV docker-compose up
+DEPLOYMENT_ENV=$DEPLOYMENT_ENV docker-compose up -d
