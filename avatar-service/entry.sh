241944f00df0373e01f424725029c0b51efb9b5e
Pao Perez <pao.pacheco.perez@gmail.com>
2020-10-14 10:50:35 +0800

    Fix: passed mongo creds not resolved

--- avatar-service/entry.sh
+++ avatar-service/entry.sh
@@ -3,4 +3,4 @@
 MONGO_USERNAME=$(cat /data/secrets/mongo_username)
 MONGO_PASSWORD=$(cat /data/secrets/mongo_password)
 
-exec java ${JAVA_OPTS} -cp "/usr/app:/usr/app/lib/*" "com.paoperez.avatarservice.MainApplication" --MONGO_USERNAME=$MONGO_USERNAME --MONGO_PASSWORD=$MONGO_PASSWORD ${@}
+exec java ${JAVA_OPTS} -cp "/usr/app:/usr/app/lib/*" "com.paoperez.avatarservice.MainApplication" --MONGO_USERNAME=${MONGO_USERNAME} --MONGO_PASSWORD=${MONGO_PASSWORD} ${@}
