abe5981fe8db257af502c8725892ab42dc885b18
Pao Perez <pao.pacheco.perez@gmail.com>
2020-10-17 12:43:43 +0800

    cleanup: no longer need boot deploy script

--- boot-startup-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit as soon as an error is encountered
-set -e
-
-# Get environment values from Metadata server
-DEPLOYMENT_ENV=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/deployment-env" -H "Metadata-Flavor: Google")
-
-DEPLOYMENT_ENV=$DEPLOYMENT_ENV docker-compose up -d
