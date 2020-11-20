2a8a50e87f828aa5db078a5cf71e4135b522d437
Pao Perez <pao.pacheco.perez@gmail.com>
2020-11-21 04:53:35 +0800

    temp fix: allow cors for web frontend for other env too

--- graphql-service/src/main/java/com/paoperez/graphqlservice/GraphQLConfig.java
+++ graphql-service/src/main/java/com/paoperez/graphqlservice/GraphQLConfig.java
@@ -14,14 +14,28 @@ import java.net.URL;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class BaseConfig {
-  private static final Logger logger = LoggerFactory.getLogger(BaseConfig.class);
+class GraphQLConfig {
+  private static final Logger logger = LoggerFactory.getLogger(GraphQLConfig.class);
+
+  @Bean
+  WebMvcConfigurer corsConfigurer(@Value("${client.web.address}") String clientWebAddress) {
+    logger.info("Allowing cross-origin requests for /graphql from origin: " + clientWebAddress);
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/graphql").allowedOrigins(clientWebAddress);
+      }
+    };
+  }
 
   @LoadBalanced
   @Bean
