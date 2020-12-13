0342dacbd084be3484ea479b431bb47714284694
Pao Perez <pao.pacheco.perez@gmail.com>
2020-12-13 18:17:06 +0000

    refactor avatar-service: change volume mapping, bind mount secrets instead

--- avatar-service/standalone-setup.sh
+++ avatar-service/standalone-setup.sh
@@ -4,20 +4,11 @@
 set -e
 
 DEPLOYMENT_ENV=standalone
-ROOT_DIR=/mnt/disks/$DEPLOYMENT_ENV-contentually
 SERVICE=avatar
-APP_DIR=./..
 
-# Create service log dir
-mkdir -m 777 -p $ROOT_DIR/$SERVICE/service/log
 # Build service container image
-DOCKER_BUILDKIT=1 docker build -t $SERVICE-service:0.0.1 .
-# Setup db data/log and secrets directories
-mkdir -p $ROOT_DIR/$SERVICE/db/data $ROOT_DIR/$SERVICE/secrets
-mkdir -m 777 -p $ROOT_DIR/$SERVICE/db/log
-# Setup Mongo db access
-cat $APP_DIR/secrets/mongo_username > $ROOT_DIR/$SERVICE/secrets/mongo_username
-cat $APP_DIR/secrets/mongo_password > $ROOT_DIR/$SERVICE/secrets/mongo_password
+docker build -t $SERVICE-service:0.0.1 .
+
 # Build db container image
 cd db/ && docker build -t $SERVICE-db:0.0.1 . && cd -
 
