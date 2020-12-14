716898370dc445a7fc9cd9ced24b698707c09ba1
Pao Perez <pao.pacheco.perez@gmail.com>
2020-12-15 04:32:42 +0800

    refactor: change volume type/mapping, accessing secrets for category service, cleanup script

--- category-service/db/scripts/init.sh
+++ category-service/db/scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-MONGO_USERNAME=$(cat /data/secrets/mongo_username)
-MONGO_PASSWORD=$(cat /data/secrets/mongo_password)
+MONGO_USERNAME=$(cat /mnt/disks/secrets/mongo_username)
+MONGO_PASSWORD=$(cat /mnt/disks/secrets/mongo_password)
 
 echo "Creating user ${MONGO_USERNAME} with readWrite access to database contentually..."
 mongo admin --eval "db.createUser({ user: '${MONGO_USERNAME}', pwd: '${MONGO_PASSWORD}', roles: [{ role: 'readWrite', db: 'contentually' }]});"
--- category-service/entry.sh
+++ category-service/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MONGO_USERNAME=$(cat /data/secrets/mongo_username)
-MONGO_PASSWORD=$(cat /data/secrets/mongo_password)
+MONGO_USERNAME=$(cat /mnt/disks/secrets/mongo_username)
+MONGO_PASSWORD=$(cat /mnt/disks/secrets/mongo_password)
 
 exec java ${JAVA_OPTS} -cp "/usr/app:/usr/app/lib/*" "com.paoperez.categoryservice.MainApplication" --MONGO_USERNAME=${MONGO_USERNAME} --MONGO_PASSWORD=${MONGO_PASSWORD} ${@}
