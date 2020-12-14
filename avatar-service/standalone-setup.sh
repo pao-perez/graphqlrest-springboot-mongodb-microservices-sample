61743670760c5186d9602582fb8362c3c4a94450
Pao Perez <pao.pacheco.perez@gmail.com>
2020-12-15 04:31:47 +0800

    housekeeping: cleanup volumes too

--- avatar-service/standalone-setup.sh
+++ avatar-service/standalone-setup.sh
@@ -19,5 +19,6 @@ cd db/ && docker build -t ${SERVICE}-db:0.0.1 . && cd -
 DEPLOYMENT_ENV=${DEPLOYMENT_ENV} docker-compose up
 
 docker-compose down
+docker volume prune
 docker volume rm ${SERVICE}-service_servicelog
 rm -r ${ROOT_DIR}
