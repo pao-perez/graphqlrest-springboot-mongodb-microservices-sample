07eee510f772e0f75e404966b459b5eb6ef3fdcb
Pao Perez <pao.pacheco.perez@gmail.com>
2020-12-16 04:38:16 +0800

    change logs volume path

--- content-service/standalone-setup.sh
+++ content-service/standalone-setup.sh
@@ -5,10 +5,10 @@ set -e
 
 DEPLOYMENT_ENV=standalone
 SERVICE=content
-ROOT_DIR=/mnt/disks/${DEPLOYMENT_ENV}-contentually/${SERVICE}
+APP_LOGS_DIR=/mnt/disks/${DEPLOYMENT_ENV}-contentually/spring/logs
 
 # Create spring log dir
-mkdir -m 777 -p ${ROOT_DIR}/logs
+mkdir -m 777 -p ${APP_LOGS_DIR}
 
 # Build service container image
 DOCKER_BUILDKIT=1 docker build -t $SERVICE-service:0.0.1 .
@@ -21,4 +21,4 @@ DEPLOYMENT_ENV=${DEPLOYMENT_ENV} docker-compose up
 docker-compose down
 docker volume prune --force
 docker volume rm ${SERVICE}-service_servicelog
-rm -r ${ROOT_DIR}
+rm -r ${APP_LOGS_DIR}
